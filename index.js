72af716b0319b258eeafce478c8613553058771d
Josue Diaz <josue.diaz@mindera.com>
2021-04-20 12:45:32 +0100

    Add functionality to form

---
+++ index.js
@@ -0,0 +1,97 @@
+(function () {
+  const form = document.querySelector(".newsletter-form");
+  const newsletters = document.querySelectorAll(".newsletter");
+  const email = form.querySelector("#email-input");
+  const agreementCheckbox = form.querySelector("#subscribe-agreement-input");
+  const submitButton = form.querySelector("#submit-button");
+  const emailValidator = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const newslettersToReceive = {
+    email: "",
+    newsletters: [],
+  };
+
+  /**
+   * Add event Listeners
+   */
+  agreementCheckbox.addEventListener("change", (e) => {
+    handleEnableSubmitButton(e.target.checked);
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!emailValidator.test(email.value)) {
+      alert("please enter valid email");
+      return;
+    }
+    if (email.value.length > 0) {
+      if (hasNewsletters() > 0) {
+        handleSubmission(email.value);
+      } else {
+        alert("please select a newsletter");
+      }
+    } else {
+      alert("please enter email");
+    }
+  });
+
+  /**
+   * Toggles button from enabled to disabled
+   */
+  function handleEnableSubmitButton(isEnabled) {
+    if (isEnabled) {
+      submitButton.disabled = false;
+      toggleSubmitButton();
+    } else {
+      submitButton.disabled = true;
+      toggleSubmitButton();
+    }
+  }
+
+  /**
+   * Toggles submit button class name
+   */
+  function toggleSubmitButton() {
+    submitButton.classList.toggle("disabled-button");
+  }
+
+  /**
+   * Checks if there are any newsletters selected
+   */
+  function hasNewsletters() {
+    let hasLetter = 0;
+    newsletters.forEach((newsletter) => {
+      if (newsletter.querySelector("input").checked) {
+        hasLetter++;
+      }
+    });
+    return hasLetter;
+  }
+
+  /**
+   * Adds newsletter
+   */
+  function addNewsletter(name, description) {
+    newslettersToReceive.newsletters.push({
+      name,
+      description,
+    });
+  }
+
+  /**
+   * Handles form submission
+   */
+  function handleSubmission(emailValue) {
+    newslettersToReceive.email = emailValue;
+    newsletters.forEach((newsletter) => {
+      if (newsletter.querySelector("input").checked) {
+        addNewsletter(
+          newsletter.querySelector("h3").innerText,
+          newsletter.querySelector("p").innerText
+        );
+      }
+    });
+    alert(JSON.stringify(newslettersToReceive, null, 4));
+    toggleSubmitButton();
+    form.reset();
+  }
+})();
